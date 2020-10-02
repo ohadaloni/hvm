b3b8feb92ba0d956634daf9243f11030a2024adc
ohad <ohad@theora.com>
2020-10-02 18:02:08 +0300

    SnapShot

--- src/index.php
+++ src/index.php
@@ -5,6 +5,17 @@ require_once(M_DIR."/mfiles.php");
 require_once("hvmFiles.php");
 require_once("Hvm.class.php");
 /*------------------------------------------------------------*/
+$ua = @$_SERVER['HTTP_USER_AGENT'];
+if (
+	! $ua
+	|| stristr($ua, "bot")
+	|| stristr($ua, "crawl")
+	|| stristr($ua, "spider")
+	) {
+	http_response_code(204);
+	exit;
+}
+/*------------------------------------------------------------*/
 global $Mview;
 global $Mmodel;
 $Mview = new Mview;
